92e3b5d1a6201cb3835058f64631ba70178de6c7
shashankanp <shashanknp02@gmail.com>
2023-05-19 23:12:14 +0530

    Slider doneeeeeeeeeee

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import ComingSoon from "../components/ComingSoon";
 import Noti from "../components/Noti";
 import Card from "../components/Card";
+import Slider from "../components/Slider";
 
 export default function Home() {
   return (
     <main className="">
-      <ComingSoon />
+      <Slider />
+      {/* <ComingSoon /> */}
       {/* <Noti /> */}
     </main>
   );
