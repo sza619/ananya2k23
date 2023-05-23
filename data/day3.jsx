051b10cb5485236c521caa716455898cc6230513
shashankanp <shashanknp02@gmail.com>
2023-05-23 10:46:07 +0530

    Day3 done

---
+++ data/day3.jsx
@@ -0,0 +1,46 @@
+export default [
+  {
+    id: 1,
+    about: "NA",
+    img: "sm.jpg",
+    title: "GROUP SONG",
+    entry_Fee: "NA",
+    type: "Group",
+    points: "10 points for winners",
+    rules:
+      "Participation: Only 1 team from a group(Any number of participants), Duration: 15 minutes (max), Any language can be used, Karaoke’s can be used, Vulgarity in lyrics will lead to disqualification, Decisions of panel of judges will be final and binding.",
+  },
+  {
+    id: 2,
+    about: "NA",
+    img: "sm.jpg",
+    title: "GROUP DANCE",
+    entry_Fee: "NA",
+    type: "Group",
+    points: "10 points for winners",
+    rules:
+      "Participation: Only 1 team from a group(Any number of participants), Duration: 8+2 minutes (max), Any concept based dance can be done, If Vulgarity and defamation used in dance will lead to disqualification, Decisions of panel of judges will be final and binding.",
+  },
+  {
+    id: 3,
+    about: "NA",
+    img: "sm.jpg",
+    title: "RAMP WALK",
+    entry_Fee: "NA",
+    type: "Group",
+    points: "10 points for winners",
+    rules:
+      "Participation: Only 1 team from a group(Any number of participants), Duration: 10+2 minutes (max), Any theme based ramp walk can be performed, If Vulgarity and defamation used during walk will lead to disqualification, Decisions of panel of judges will be final and binding.",
+  },
+  {
+    id: 4,
+    about: "NA",
+    img: "sm.jpg",
+    title: "DRAMA",
+    entry_Fee: "NA",
+    type: "Group",
+    points: "10 points for winners",
+    rules:
+      "Participation: Only 1 team from a group(Any number of participants), Duration: 10+5 minutes (max), Any concept based drama can be done, If Vulgarity and defamation used in drama will lead to disqualification, Decisions of panel of judges will be final and binding.",
+  },
+];
--- src/app/events/day3/page.jsx
+++ src/app/events/day3/page.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import Image from "next/image";
+import Card from "../../../components/Card";
+import EventComp from "../../../components/EventComp";
+import group from "../../../../data/day3";
 
 export default function Day3() {
+  const cardss = group.map((itemm) => {
+    return (
+      <Card
+        key={itemm.id}
+        title={itemm.title}
+        img={itemm.img}
+        day={itemm.day}
+        about={itemm.about}
+        fee={itemm.entry_Fee}
+        points={itemm.points}
+        rules={itemm.rules}
+      />
+    );
+  });
+
   return (
     <div>
-      <h1 className="text-3xl my-5">Day 3</h1>
+      <section>
+        <h1 className="text-5xl text-center mt-5 mb-2">ANANYA DAY</h1>
+        <br />
+        <div className="cards-list container">{cardss}</div>
+      </section>
     </div>
   );
 }
