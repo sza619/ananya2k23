0f9cc61fe5c072a71c1a9b65f339d278ece5cffd
SanjeevMysore <sanjeevmysore1@gmail.com>
2023-05-23 18:10:16 +0530

    Header mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ import Image from "next/image";
 export default function Header() {
   return (  
     <header className="head">
-      <Image src={logo} alt="Image" priority className="mx-1 w-12 h-12 md:w-36 md:h-36" />{" "}
+      <Image src={logo} alt="Image" priority className="mx-1 w-10 h-10 md:w-36 md:h-36" />{" "}
       {/* <h1>ANANYA 2K23</h1> */}
-      <Image src={ananya} alt="Image" priority className="mx-1 w-56 h-16 md:w-1/2 md:h-36" />{" "}
-      <Image src={axi23} alt="Image" priority className="mx-1 w-40 h-16 md:w-64 md:h-36" />{" "}
-      <Image src={ananya_logo} alt="Image" priority className="mx-1 w-12 h-12 md:w-40 md:h-36" />{" "}
+      <Image src={ananya} alt="Image" priority className="mx-1 w-40 h-12 md:w-1/2 md:h-36" />{" "}
+      <Image src={axi23} alt="Image" priority className="mx-1 w-24 h-12 md:w-64 md:h-36" />{" "}
+      <Image src={ananya_logo} alt="Image" priority className="mx-1 w-10 h-10 md:w-40 md:h-36" />{" "}
     </header>
   );
 }
