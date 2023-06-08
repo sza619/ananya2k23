03d0d40fe0d9e0e16b8f1dbbbc5a588317f54013
shashankanp <shashanknp02@gmail.com>
2023-06-08 11:44:18 +0530

    Final V2

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,9 +4,25 @@ export default function Days() {
   return (
     <section>
       <h1 className="text-xl md:text-3xl text-center my-5 g">About KSIT</h1>
-      <h1 className="text-sm mx-5 flex justify-evenly md:text-base">Established in 2010, K.S. Institute of Technology is a premier Institute located in Karnataka. A leading institute in the city Degree and Diploma courses including 17 UG, 7 PG and 5 after 10th courses and programs, that are designed to meet the needs of students from different backgrounds. KSIT offer courses like B.E. / B.Tech, B.Arch, M.E./M.Tech, MBA/PGDM, after Diploma with stream of Engineering, Architecture & Planning, IT & Software, Business & Management Studies. K.S. Group of Institutions offered courses that are recognized by approving bodies like AICTE.</h1>
+      <h1 className="text-sm mx-5 flex justify-evenly md:text-base">
+        Established in 2010, K.S. Institute of Technology is a premier Institute
+        located in Karnataka. A leading institute in the city Degree and Diploma
+        courses including 17 UG, 7 PG and 5 after 10th courses and programs,
+        that are designed to meet the needs of students from different
+        backgrounds. KSIT offer courses like B.E. / B.Tech, B.Arch, M.E./M.Tech,
+        MBA/PGDM, after Diploma with stream of Engineering, Architecture &
+        Planning, IT & Software, Business & Management Studies. K.S. Group of
+        Institutions offered courses that are recognized by approving bodies
+        like AICTE.
+      </h1>
       <h1 className="text-xl md:text-3xl text-center my-5 g">About ANANYA</h1>
-      <h1 className="text-sm mx-5 flex justify-evenly md:text-base">Festivals are celebrated to bring the connection of many untold stories and relish the moments from before. Celebrations give rise to new memories that stay for a lifetime. One such festival, "ANANYA," is on its way. Let's mark this as the beginning of the festival, come together, and celebrate !!</h1>
+      <h1 className="text-sm mx-5 flex justify-evenly md:text-base">
+        Festivals are celebrated to bring the connection of many untold stories
+        and relish the moments from before. Celebrations give rise to new
+        memories that stay for a lifetime. One such festival,
+        &ldquo;ANANYA,&ldquo; is on its way. Let&quot;s mark this as the
+        beginning of the festival, come together, and celebrate !!
+      </h1>
     </section>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main className="">
       <section>
-        <h1 className="text-sm md:text-3xl text-center my-5 g">K S Institute of Technology is proud to present its Annual Cultural Extravaganza, "ANANYA"
+        <h1 className="text-sm md:text-3xl text-center my-5 g">
+          K S Institute of Technology is proud to present its Annual Cultural
+          Extravaganza, &ldquo;ANANYA&ldquo;
         </h1>
       </section>
       <Slider />
